2395417510c9ea50115ca5f57d68badee05df2d7
Tyler Rosnett <tylerrosnett@Tylers-MBP.local>
2022-02-26 15:15:13 -0600

    fixing cat save

--- src/commands/catme.js
+++ src/commands/catme.js
@@ -7,20 +7,24 @@ module.exports = {
   async execute(msg) {
 
     let cataas = new Cataas();
-
     cataas.encode();
+
+    console.log('getting cat');
+
     cataas.get()
       .then(readable => {
-        const stream = new fs.createWriteStream('cat.png');
+        console.log('writing cat to fs');
+        const stream = new fs.createWriteStream('./cat.png');
         readable.pipe(stream);
       })
       .catch(e => console.error(e));
+
+    console.log('sending cat message');
 
     await msg.channel.send({
       files: [
         './cat.png'
       ]
     });
-    fs.rmSync('./cat.png');
   },
 };
