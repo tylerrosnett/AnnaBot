b97a240186415ec5a3a01169e276228bc2c8f4f8
Tyler Rosnett <tylerrosnett@gmail.com>
2022-02-26 21:37:25 -0600

    added ducky command

---
+++ src/commands/ducky.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const https = require('https');
+
+const url = new URL('https://random-d.uk/api/v2/randomimg');
+
+module.exports = {
+  name: 'ducky',
+  description: 'Responds with a random cat picture.',
+  aliases: ['d'],
+  async execute(msg) {
+
+    const file = fs.createWriteStream('./duck.jpg');
+
+    https.get(url, res => {
+
+      if (res.statusCode !== 200) {
+        console.log(new Error(`Request Failed.\nStatus Code: ${res.statusCode}`));
+      } else {
+
+        res.pipe(file).on('finish', () => {
+          file.close();
+          msg.reply({
+            files: ['./duck.jpg']
+          });
+        });
+
+      }
+    });
+  }
+};
